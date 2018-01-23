d94f3c44c717d960888a9f5d51186e5a0bd2309b
Roy Norton <rnorton12@centurylink.net>
2018-01-22 20:09:26 -0700

    making progress

---
+++ hangman.js
@@ -0,0 +1,100 @@
+var word = require("./word.js");
+var prompt = require('prompt');
+
+var theWord = new word();
+
+var gameStats = {
+    guessesRemaining: 8,
+    Wins: 0,
+    Losses: 0,
+
+    printStats: function () {
+        console.log("Wins: " + this.Wins);
+        console.log("Losses: " + this.Losses);
+    }
+};
+
+//
+// Start the prompt
+//
+prompt.start();
+
+function askForNewGame() {
+        prompt.get([{
+        name: 'yesno', 
+        message: 'Play Again? Yes or No', 
+        validator: /y[es]*|n[o]?/, 
+        warning: 'Must respond yes or no', 
+        required: true
+    }], function (err, result) {
+        if (err) {
+            throw error;
+        }
+        console.log(result.yesno[0]);
+        if (result.yesno[0].toLowerCase() === 'y') {
+            console.log("new game: " + "true");
+            return true;
+        } else {
+            console.log("new game: " + "false");
+            return false;
+        }        
+    });
+}
+
+function askForLetter() {
+    prompt.get([{
+        description: 'Enter a letter',
+        name: 'letter',
+        pattern: /^[a-zA-Z]+$/,  // allow only letters
+        message: 'Enter only letters!',
+        required: true
+    }], function (err, result) {
+        if (err) {
+            throw err;
+        }
+        if (theWord.checkGuessedLetter(result.letter[0])) {
+            console.log(theWord.getUnsolvedWord());
+            console.log("CORRECT!");
+            if (theWord.isWordSolved()) {
+                console.log("YOU WIN!");
+                gameStats.Wins++;
+                gameStats.printStats();
+               
+                if (askForNewGame() === true) {
+                    theWord.startTheGame();
+                    console.log(theWord.getUnsolvedWord());
+                    gameStats.guessesRemaining = 8;
+                }
+                console.log("outta here");
+            } else {
+                askForLetter();
+            }
+        } else {
+            console.log("INCORRECT!");
+            console.log(theWord.getUnsolvedWord());
+            gameStats.guessesRemaining--;
+            console.log("Guesses Remaining: " + gameStats.guessesRemaining);
+            if (gameStats.guessesRemaining) {
+                askForLetter();
+            } else {
+                console.log("YOU LOST!");
+                gameStats.Losses++;
+                gameStats.printStats();
+                
+                if (askForNewGame() === true) {
+                    theWord.startTheGame();
+                    console.log(theWord.getUnsolvedWord());
+                    gameStats.guessesRemaining = 8;
+                }
+            }
+        }
+        
+    });
+}
+
+theWord.startTheGame();
+console.log(theWord.getUnsolvedWord());
+console.log("Guesses Remaining: " + gameStats.guessesRemaining);
+askForLetter(); 
+
+　
--- word.js
+++ word.js
@@ -7,28 +7,53 @@ var Word = function() {
     this.solved = false;
 }
 
-Word.prototype.setTheWordToSolve = function () {
+Word.prototype.startTheGame = function () {
+    // initialize object
+    this.wordToSolve = "";
+    this.letterArray = [];
+    this.solved = false;
+
+    // get a game word to solve
     var theWord = new gameWords();
     this.wordToSolve = theWord.getRandomWord();
     for (var i = 0; i < this.wordToSolve.length; i++) {
-        var letter = new aLetter(this.wordToSolve[i]);
+        var letter = new aLetter(this.wordToSolve[i].toLowerCase());
+        // if the word contains a space then initialize guessed to true
+        if (this.wordToSolve[i].toLowerCase() === " ") {
+            letter.guessed = true;
+        }
         this.letterArray.push(letter);
     }
 };
 
 // test if this is a good letter
-Word.prototype.checkGuessedLetter = function(letter) {
-    var outputText = "";
+Word.prototype.checkGuessedLetter = function (letter) {
+    var result = false;
     for (var i = 0; i < this.wordToSolve.length; i++) {
-        outputText += this.letterArray[i].checkLetter(letter) + " ";
-        console.log(this.letterArray[i]);
+        if (this.letterArray[i].letter === letter.toLowerCase()) {
+            result = true;
+            this.letterArray[i].guessed = result;
+        }
     }
-    return (outputText);
+    return result;
+}
+
+Word.prototype.getUnsolvedWord = function () {
+    var result = "";
+    for (var i = 0; i < this.wordToSolve.length; i++) {
+        if (this.letterArray[i].guessed) {
+            result += this.letterArray[i].letter + " ";
+        }
+        else {
+            result += "_" + " ";
+        }
+    }
+    return result;
 }
 
 Word.prototype.isWordSolved = function() {
     for (var i = 0; i < this.wordToSolve.length; i++) {
-        if (this.wordToSolve[i].guessed === false) {
+        if (this.letterArray[i].guessed === false) {
             this.solved = false;
             break;
         } else {
@@ -40,12 +65,19 @@ Word.prototype.isWordSolved = function() {
 
 module.exports = Word;
 
-// module test code
-var word = new Word();
-word.setTheWordToSolve();
+// // module test code
+// var word = new Word();
+// word.startTheGame();
+// console.log(word.wordToSolve);
+// console.log(word.letterArray);
+// console.log(word.solved);
+// console.log(word.getUnsolvedWord());
 
-for (var i = 0; i < word.wordToSolve.length; i++) {
-    console.log(word.checkGuessedLetter(word.wordToSolve[i]));
-}
+// for (var i = 0; i < word.wordToSolve.length; i++) {
+//     console.log(word.wordToSolve[i]);
+//     console.log(word.checkGuessedLetter(word.wordToSolve[i]));
+//     console.log(word.letterArray);
+//     console.log(word.getUnsolvedWord());
+// }
+// console.log("solved: " + word.isWordSolved());
 
-console.log(word.isWordSolved());
